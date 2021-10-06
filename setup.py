a6fd53ce0c933af5c727760e19bb7ec6b26c8451
Pablo-Valverde <73491500+Pablo-Valverde@users.noreply.github.com>
2021-10-06 22:42:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='AuxFunctions',
+    name='auxfunctions',
     version='0.0.0',
     author='Pablo Valverde',
     description='Functions I usually used at my projects',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/Pablo-Valverde/AuxFunctions',
+    url='https://github.com/Pablo-Valverde/auxfunctions',
     license='GNU AGPLv3',
 )
