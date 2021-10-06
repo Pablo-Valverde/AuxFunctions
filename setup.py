0714d590f647efc718784f21bd9b823dbb3a4b84
Pablo-Valverde <73491500+Pablo-Valverde@users.noreply.github.com>
2021-10-06 21:04:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,5 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/Pablo-Valverde/AuxFunctions',
-    license='MIT',
-    packages=['AuxFunctions'],
+    license='GNU AGPLv3',
 )
