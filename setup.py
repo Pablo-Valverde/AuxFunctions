30ad5553f9e127d5b5ebcd4f20534695aaff5882
Pablo-Valverde <73491500+Pablo-Valverde@users.noreply.github.com>
2021-10-06 21:01:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='toolbox',
+    name='AuxFunctions',
     version='0.0.3',
-    author='Mike Huls',
-    author_email='mike_huls@hotmail.com',
-    description='Testing installation of Package',
+    author='Pablo Valverde',
+    description='Functions I usually used at my projects',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/mike-huls/toolbox',
-    project_urls = {
-        "Bug Tracker": "https://github.com/mike-huls/toolbox/issues"
-    },
+    url='https://github.com/Pablo-Valverde/AuxFunctions',
     license='MIT',
-    packages=['toolbox'],
-    install_requires=['requests'],
+    packages=['AuxFunctions'],
 )
